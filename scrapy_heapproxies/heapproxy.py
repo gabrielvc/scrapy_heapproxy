ae6a876479844e7f2c3c3811e6847ee62ec1a1f7
gabrielvc <gvictorinocardoso@gmail.com>
2018-02-01 15:35:35 +0100

    Now working with proxies without passwords

--- scrapy_heapproxies/heapproxy.py
+++ scrapy_heapproxies/heapproxy.py
@@ -1,4 +1,3 @@
-
 import re
 import random
 import base64
@@ -8,23 +7,25 @@ import datetime
 import heapq
 import pdb
 
-log = logging.getLogger('scrapy.heapproxies')
-
 
 class Mode:
     PROXY_TIMEOUT = range(1)
 
 
 class HeapProxy(object):
+    
     def __init__(self, settings):
+        super(HeapProxy, self).__init__()
         self.mode = settings.get('PROXY_MODE')
         self.proxy_list = settings.get('PROXY_LIST')
         self.timeout = float(settings.get('PROXY_TIMEOUT'))
         self.chosen_proxy = ''
+        self.logger = logging.getLogger('scrapy.heapproxies')
 
         if self.proxy_list is None:
             raise KeyError('PROXY_LIST setting is missing')
         self.proxies = {}
+        self.logger.debug('Reading file {}'.format(self.proxy_list))
         fin = open(self.proxy_list)
         try:
             for line in fin.readlines():
@@ -41,12 +42,17 @@ class HeapProxy(object):
                 self.proxies[parts.group(1) + parts.group(3)] = user_pass
         finally:
             fin.close()
-            
+        self.logger.debug("Proxies being used {}".format(self.proxies))
         now = datetime.datetime.now()
+        self.logger.debug("Building heap")
         self.proxies = [(now - datetime.timedelta(seconds = self.timeout), i, self.proxies[i]) for i in self.proxies]
         heapq.heapify(self.proxies)
+
+        self.logger.debug("Picking first proxy")
         first = heapq.heappop(self.proxies)
         self.chosen_proxy = first
+
+        self.logger.debug("Repushing proxy to end of queue")
         heapq.heappush(self.proxies, (datetime.datetime.now(), first[1], first[2]))
         
         
@@ -63,15 +69,18 @@ class HeapProxy(object):
         if len(self.proxies) == 0:
             raise ValueError('All proxies are unusable, cannot proceed')
 
+        self.logger.debug('Picking proxies')
         first = heapq.heappop(self.proxies)
         now = datetime.datetime.now()
         diff = (now - first[0]).total_seconds()
 
         if diff < self.timeout:
-            log.info("Timeout reached, waiting {} seconds".format(self.timeout - diff))
+            self.logger.info("Timeout reached, waiting {} seconds".format(self.timeout - diff))
             time.sleep(self.timeout - diff)
             
         self.chosen_proxy = first
+
+        self.logger.debug('Pushing back to heap')
         heapq.heappush(self.proxies, (datetime.datetime.now(), first[1], first[2]))
 
         proxy_address = self.chosen_proxy[1]
@@ -83,10 +92,13 @@ class HeapProxy(object):
             basic_auth = 'Basic ' + base64.b64encode(proxy_user_pass.encode()).decode()
             request.headers['Proxy-Authorization'] = basic_auth
         else:
-            log.debug('Proxy user pass not found')
-        log.debug('Using proxy <%s>, %d proxies left' % (
+            self.logger.debug('Proxy user pass not found, adding proxy without password')
+            request.meta['proxy'] = proxy_address
+            
+        self.logger.debug('Using proxy <%s>, %d proxies left' % (
                 proxy_address, len(self.proxies)))
 
+        
     def process_exception(self, request, exception, spider):
         if 'proxy' not in request.meta:
             return
@@ -94,7 +106,7 @@ class HeapProxy(object):
         try:
             for i in self.proxies:
                 if i[1] == self.chosen_proxy[i]:
-                    log.info("Proxy {} not responding, taking off".format(i[1]))
+                    self.logger.info("Proxy {} not responding, taking off".format(i[1]))
                     self.proxies.remove(i)
                     break
                 
@@ -102,5 +114,5 @@ class HeapProxy(object):
         except KeyError:
             pass
         request.meta["exception"] = True
-        log.info('Removing failed proxy <%s>, %d proxies left' % (
+        self.logger.info('Removing failed proxy <%s>, %d proxies left' % (
             proxy, len(self.proxies)))
