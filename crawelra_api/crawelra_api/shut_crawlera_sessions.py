c0e3c094ad15eee68b23d4d363915d6ae2bd9897
gabrielvc <gvictorinocardoso@gmail.com>
2018-08-22 16:46:37 +0200

    adding new functionality

--- crawelra_api/crawelra_api/shut_crawlera_sessions.py
+++ crawelra_api/crawelra_api/shut_crawlera_sessions.py
@@ -2,9 +2,9 @@ import requests
 import base64
 from w3lib.http import basic_auth_header
 import json
+from authentification import *
 
 url = 'http://proxy.crawlera.com:8010'
-api_key = "397c088c60a04cdc84624c4363e0d496:"
 headers = {
     "Authorization": basic_auth_header(api_key, '')
 }
